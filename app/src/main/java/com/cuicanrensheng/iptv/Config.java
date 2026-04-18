4feaf71e222361c4035cb33c7db83ebfc8f23ba2
cuicanrensheng <1693728915@qq.com>
2026-04-18 15:59:19 +0800

    Update Config.java

--- app/src/main/java/com/cuicanrensheng/iptv/Config.java
+++ app/src/main/java/com/cuicanrensheng/iptv/Config.java
@@ -1,7 +1,7 @@
 public class Config {
 
     // 你指定的远程PHP源
-    public static final String ONLINE_URL = "https://raw.githubusercontent.com/cuicanrensheng/IPTV/refs/heads/main/666aaa.php";
+    public static final String ONLINE_URL = "https://gitee.com/qf_1111/iptv/raw/master/playlist%20.m3u";
 
     // APP内置本地源（默认启动用这个）
     public static final String LOCAL_URL = "file:///android_asset/channel.txt";

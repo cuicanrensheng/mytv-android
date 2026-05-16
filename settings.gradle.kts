a4e4acc28d0ed8cfb0e64f0f0a6916278376445a
cuicanrensheng <1693728915@qq.com>
2026-05-16 13:19:36 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,18 +2,13 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
     }
 }
 
 dependencyResolutionManagement {
-    // 改回 PREFER_SETTINGS，让全局仓库生效
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-    }
+    // 关键：强制优先使用 app 项目内的仓库
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    // 彻底删除全局 repositories 块，不再提供全局仓库
 }
 
 rootProject.name = "myTV-Android"

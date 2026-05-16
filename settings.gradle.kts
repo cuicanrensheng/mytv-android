ea0dcf77119f6fe686b9f4f71448aaf217c028c0
cuicanrensheng <1693728915@qq.com>
2026-05-16 12:57:34 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,5 @@
 pluginManagement {
     repositories {
-        // 优先用公共库，避免阿里云Google源502
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
     }
@@ -10,11 +8,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
     }
 }
-
-rootProject.name = "myTV-Android"
-include(":app")

c0fd421b26d22694c6c3000fe608a3190f109bb6
cuicanrensheng <1693728915@qq.com>
2026-05-16 14:05:23 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.kotlin.serialization) apply (false)
+    alias(libs.plugins.kotlin.serialization) apply false
+}
+
+// 全局强制替换 xmlpull 版本，优先级最高，彻底屏蔽 1.1.3.1
+allprojects {
+    configurations.all {
+        resolutionStrategy.force("org.xmlpull:xmlpull:1.1.3.4")
+    }
 }

54473070f62447efe8160676b911a6f9bd3188f9
cuicanrensheng <1693728915@qq.com>
2026-05-16 12:32:01 +0800

    Update BootReceiver.kt

--- app/src/main/java/top/yogiczy/mytv/BootReceiver.kt
+++ app/src/main/java/top/yogiczy/mytv/BootReceiver.kt
@@ -1,13 +1,13 @@
-package com.tviptv.receiver
+package top.yogiczy.mytv
 
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.tviptv.MainActivity
+import top.yogiczy.mytv.activities.MainActivity
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (Intent.ACTION_BOOT_COMPLETED == intent?.action) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent?.action && top.yogiczy.mytv.data.SettingManager.bootStart) {
             val i = Intent(context, MainActivity::class.java)
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             context?.startActivity(i)

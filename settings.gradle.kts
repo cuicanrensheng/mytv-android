dafeeb854d4bb0828dd0f1cb3e59204c04829068
cuicanrensheng <1693728915@qq.com>
2026-05-16 13:05:35 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,10 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+    // 改为优先使用项目仓库，不再强制用settings仓库
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    // 删除全局repositories块，完全交给app模块自己配置
 }
+
+rootProject.name = "myTV-Android"
+include(":app")
